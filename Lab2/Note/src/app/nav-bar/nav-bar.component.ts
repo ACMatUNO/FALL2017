312584d5b4a39451b7ad421c697b9ff199628a56
Christian <ravenjolly07@gmail.com>
2017-10-09 11:42:31 -0500

    added git ignore

---
+++ Lab2/Note/src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,13 @@
+
+import {Component} from "@angular/core";
+
+@Component({
+
+selector: 'navvybar',
+  templateUrl : './nav-bar.component.html',
+  styleUrls: ['nav-bar.component.css'] //notice how this is an array
+
+})
+
+
+export class NavvybarComponent { }
